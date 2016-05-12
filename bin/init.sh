89df8d3dba8534eb5ffb5437f2ba658fd7041c75
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2016-05-12 12:54:56 +0200

    Initial commit

---
+++ bin/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pushd $(dirname $0)/.. &> /dev/null
+
+echo -n "-- Updating submodules..."
+git submodule update --init --recursive
+if [ $? == 0 ]; then
+	printf "\tSuccess\n"
+else
+	printf "\tFailure\n"
+	exit 1
+fi
+
+popd &> /dev/null
