bc871fa4254c42bb7cbbf3213c2274ed5b5cc676
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2019-11-20 09:10:42 +0100

    Choose YCM install flags based on the system

--- bin/init.sh
+++ bin/init.sh
@@ -11,9 +11,31 @@ else
 	exit 1
 fi
 
-echo -n "-- Building YCM..."
-python3 ./bundle/YouCompleteMe/install.py \
-	--all
+ycm_flags=""
+
+function ycm_flag() {
+	ycm_flags="$ycm_flags $1"
+}
+
+function ycm_flag_if_cmd() {
+	cmd=$1
+	flag=$2
+	echo "-- Checking $cmd command"
+	path=$(command -v $cmd)
+	if [[ -n $path ]]; then
+		echo "-- Found: $path"
+		ycm_flags="$ycm_flags $flag"
+	else
+		echo "-- Not found"
+	fi
+}
+
+echo "-- Building YCM..."
+
+ycm_flag --clang-completer
+ycm_flag_if_cmd rustup --rust-completer
+
+python3 ./bundle/YouCompleteMe/install.py $ycm_flags
 if [ $? == 0 ]; then
 	printf "\t\tSuccess\n"
 else
